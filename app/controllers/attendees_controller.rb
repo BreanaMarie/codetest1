8765d89bf37ea35cc417d1bbb9866d9081fb2dce
BreanaMarie <msbremarie@gmail.com>
2016-01-20 14:37:57 -0800

    add attendee new and rerout to attende show page.

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -3,8 +3,17 @@ class AttendeesController < ApplicationController
   end
 
   def new
+    @attendee = Attendee.new
+    render :new
   end
 
   def create
+    attendee_params = params.require(:attendee).permit(:first_name, :last_name, :passion)
+    @attendee = Attendee.new(attendee_params)
+    if @attendee.save
+      redirect_to @attendee
+    else
+      render :new
+    end
   end
 end
