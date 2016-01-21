a869901062fe4064c35b56de52eef52c5c9b5777
BreanaMarie <msbremarie@gmail.com>
2016-01-20 16:32:33 -0800

    attendence count by talk shown on the index page.  attendance create added to the attendee create form.  create talk show page

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,8 +1,14 @@
 class AttendancesController < ApplicationController
   def new
+    @attendance = Attendance.new
+    @talks = Talk.all
+    render :new
   end
 
   def create
+    attendance_params = params.require(:attendance).permit(:talk_id, :attendee_id)
+    @attendance = Attendance.new(attendance_params)
+    @attendance.save
   end
 
   def destroy
--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -4,16 +4,26 @@ class AttendeesController < ApplicationController
 
   def new
     @attendee = Attendee.new
+    @talks = Talk.all
+    @attendances = Attendance.new
     render :new
   end
 
   def create
     attendee_params = params.require(:attendee).permit(:first_name, :last_name, :passion)
     @attendee = Attendee.new(attendee_params)
+    @talks = Talk.all
+
     if @attendee.save
+      create_attend @attendee.id, @talks_id
       redirect_to @attendee
     else
       render :new
     end
+  end
+
+  def create_attend attendee_id, talk_id
+    Attendance.create(attendee_id: attendee_id, talk_id: talk_id)
+
   end
 end
